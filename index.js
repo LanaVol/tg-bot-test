ee8e40333112607e1494b4e208747d6d909e7ace
LanaVol <svolosh4yk@gmail.com>
2024-02-01 00:58:15 +0200

    add fs for work with db & updated fs for bot events

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-const TelegramBotAPI = require("node-telegram-bot-api");
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
+import TelegramBotAPI from "node-telegram-bot-api";
+import express from "express";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import fs from "fs";
+dotenv.config();
 
+const filename = "data.json";
+
+function saveDataToJsonFile(filename, data) {
+  // const jsonData = JSON.stringify(data, null, 2);
+
+  const values = readDataFromJsonFile(filename);
+
+  const updatedValues = { ...values, ...data };
+
+  fs.writeFileSync(filename, JSON.stringify(updatedValues), "utf8");
+}
+
+function readDataFromJsonFile(filename) {
+  try {
+    const jsonData = fs.readFileSync(filename, "utf8");
+
+    return JSON.parse(jsonData);
+  } catch (error) {
+    console.error(`Error reading data from ${filename}: ${error.message}`);
+    return null;
+  }
+}
+
+const dataBase = [];
 const token = process.env.API_TOKEN_BOT;
 const bot = new TelegramBotAPI(token);
 
@@ -11,13 +37,14 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-const webhookUrl = "https://tg-bot-test-neon.vercel.app/telegram-webhook";
+const webhookUrl = "https://7721-176-37-48-101.ngrok-free.app/telegram-webhook";
+// const webhookUrl = "https://tg-bot-test-neon.vercel.app/telegram-webhook";
 
 bot.setWebHook(webhookUrl);
 
 // app.post("/telegram-webhook", (req, res) => {
 //   const { body } = req;
-//   console.log("*Received update:", body);
+
 //   bot.processUpdate(body);
 //   res.sendStatus(200).json({ message: body });
 //   console.log("Success");
@@ -40,15 +67,41 @@ bot.setWebHook(webhookUrl);
 app.post("/telegram-webhook", (req, res) => {
   const { body } = req;
   bot.processUpdate(body);
+  const dataToSave = { [body.update_id]: `${body.message.text}` };
+  saveDataToJsonFile(filename, dataToSave);
+  console.log("body: ", body);
+
+  // const chatId = body.message.chat.id;
+
+  // bot.sendMessage(
+  //   chatId,
+  //   `Your name is ${body.message.text} ${JSON.stringify(
+  //     readDataFromJsonFile(filename)
+  //   )}`
+  // );
 
   res.json({ message: body });
 });
 
+bot.on("text", (msg) => {
+  console.log("***", msg);
+  const chatId = msg.chat.id;
+  const text = msg.text;
+  if (text === "/start") {
+    bot.sendMessage(chatId, "Hello! This is Bot-tester");
+  }
+  if (text === "/info") {
+    bot.sendMessage(chatId, `You wrote ${text}`);
+  }
+  if (text) {
+    bot.sendMessage(chatId, `You wrote ${text}`);
+  }
+});
+
 app.get("/telegram-webhook", (req, res) => {
-  res.json({ message: "dljfljsd skdjfksdfkjsdkf" });
+  res.json({ message: "Hello" });
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-// bot.startPolling();
