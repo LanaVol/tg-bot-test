2cdbed9f967347e0192a8fe872221712112efdfc
LanaVol <svolosh4yk@gmail.com>
2024-01-31 17:01:16 +0200

    update with bot text

--- index.js
+++ index.js
@@ -19,25 +19,26 @@ app.post("/telegram-webhook", (req, res) => {
   const { body } = req;
   bot.processUpdate(body);
   res.sendStatus(200);
+  console.log("Success");
 });
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-// bot.on("text", async (msg) => {
-//   const text = msg.text;
-//   const chatId = msg.chat.id;
+bot.on("text", async (msg) => {
+  const text = msg.text;
+  const chatId = msg.chat.id;
 
-//   if (text === "/start") {
-//     bot.sendMessage(chatId, "Hello! This is Bot-tester");
-//   }
-//   if (text === "/info") {
-//     bot.sendMessage(
-//       chatId,
-//       `Your name is ${msg.from.first_name} ${msg.from.last_name}`
-//     );
-//   }
-// });
+  if (text === "/start") {
+    bot.sendMessage(chatId, "Hello! This is Bot-tester");
+  }
+  if (text === "/info") {
+    bot.sendMessage(
+      chatId,
+      `Your name is ${msg.from.first_name} ${msg.from.last_name}`
+    );
+  }
+});
 
 // bot.startPolling();
