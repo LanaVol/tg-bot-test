c26a6ba794daab29513b51cbe40177f62467191e
LanaVol <svolosh4yk@gmail.com>
2024-02-03 01:38:48 +0200

    firebase config & init database & work with conection user, bot, database

---
+++ src/firebaseService.js
@@ -0,0 +1,45 @@
+import { initializeApp } from "firebase/app";
+import "firebase/database";
+import {
+  child,
+  equalTo,
+  get,
+  getDatabase,
+  orderByChild,
+  query,
+} from "firebase/database";
+import { ref, set } from "firebase/database";
+import { bot } from "./botServise.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API,
+  authDomain: "trello-bot-project.firebaseapp.com",
+  databaseURL: "https://trello-bot-project-default-rtdb.firebaseio.com",
+  projectId: "trello-bot-project",
+  storageBucket: "trello-bot-project.appspot.com",
+  messagingSenderId: "294895283237",
+  appId: "1:294895283237:web:60ee4853f517890b7463cc",
+};
+
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+export const addUserToDatabase = async (chatId, userId, user) => {
+  const dbRef = ref(database, "/users");
+  const queryRef = query(dbRef, orderByChild("id"), equalTo(userId));
+
+  const snapshot = await get(queryRef);
+
+  if (snapshot.exists()) {
+    return bot.sendMessage(chatId, "✅You are already registered!");
+  } else {
+    const userRef = child(dbRef, userId.toString());
+    await set(userRef, {
+      id: userId,
+      name: `${user.first_name} ${user.last_name}`,
+    });
+  }
+};
